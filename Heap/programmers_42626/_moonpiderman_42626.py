fab9191b7727a4e97ce11b48cf9e3d169c0820f6
moonpiderman <lotsofchuck@gmail.com>
2021-05-13 13:55:53 +0900

    fin moonpiderman

---
+++ Heap/programmers_42626/_moonpiderman_42626.py
@@ -0,0 +1,24 @@
+import heapq
+def func_scoville(first, second) :
+    mix_scoville = first + (second * 2)
+    return mix_scoville
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K :
+        if len(scoville) > 1 :
+            first = heapq.heappop(scoville)
+            second = heapq.heappop(scoville)
+            heapq.heappush(scoville, func_scoville(first, second))
+            answer += 1
+        else :
+            return -1
+
+    return answer
+
+if __name__ == '__main__' :
+    scoville = [1, 2, 3, 9, 10, 12]
+    k = 7
+    print(solution(scoville, k))
