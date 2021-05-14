c56aca0b4b0b694a2162e35f7b576f39361351b1
Noaarhk <skarb8277@naver.com>
2021-05-14 14:33:44 +0900

    0514

--- Heap/leetcode_347/_noaarhk_347.py
+++ Heap/leetcode_347/_noaarhk_347.py
@@ -30,23 +30,47 @@ def solution(nums: List, k: int) -> List:
 #         _siftup(heap, 0)
 #         return returnitem
 #     return lastelt
+class Solution:
 
-def solution_2(nums, k):
-    freqs = Counter(nums)
-    freqs_heap = []
-    for f in freqs:
-        # heappop 은 가장 작은수를 뽑기 때문에 반대로 음수로 바꾼 빈도수를 해당 숫자와 튜블로 묶어서 heap 에 저장한다.
-        heapq.heappush(freqs_heap, (-freqs[f], f))
-    print(freqs_heap)
-    topk = []
+    def m1(self, nums: List[int], k: int) -> List[int]:
+        freqs = Counter(nums)
+        freqs_heap = []
+        for f in freqs:
+            # heappop 은 가장 작은수를 뽑기 때문에 반대로 음수로 바꾼 빈도수를 해당 숫자와 튜블로 묶어서 heap 에 저장한다.
+            heapq.heappush(freqs_heap, (-freqs[f], f))
+        print(freqs_heap)
+        topk = []
 
-    # k 보다 큰 빈도수를 찾아야 하기 때문에 k 번 반복?
-    for _ in range(k):
-        topk.append(heapq.heappop(freqs_heap)[1])
-    return topk
+        # k 보다 큰 빈도수를 찾아야 하기 때문에 k 번 반복?
+        for _ in range(k):
+            topk.append(heapq.heappop(freqs_heap)[1])
+        return topk
+
+
+
+    def m2(self, nums: List[int], k: int) -> List[int]:
+        # O(1) time
+        # if k == len(nums):
+        #     return nums
+
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        # nlargest ( n, iterable, key )
+        # == sorted(iterable, key=key, reverse=True)[:n]
+        # 큰것부터 n개
+        answer = heapq.nlargest(k, count.keys(), key=count.get)
+        return answer
 
 
 if __name__ == '__main__':
     nums = [1, 1, 1, 2, 2, 3]
+    nums_2 = [3, 0, 1, 0]
+    nums_3 = [1,2]
     k = 2
-    print(solution_2(nums, k))
+    solution = Solution()
+    print(solution.m1(nums_3, k))
+    print(solution.m2(nums_3, k))
---
+++ Heap/programmers_42626/noaarhk_42626.py
@@ -0,0 +1,52 @@
+import heapq
+    from collections import deque
+    from typing import List
+
+
+def scouvile(a, b):
+    return a + b * 2
+
+
+def my_solution(nums: List, k: int) -> int:
+
+    nums_dq = deque(sorted(nums))
+    cnt = 0
+
+    while nums_dq[0] < k:
+        nums_dq.append(scouvile(nums_dq.popleft(), nums_dq.popleft()))
+        cnt += 1
+
+        if len(nums_dq) == 1:
+            break
+    if nums_dq[-1] < k :
+        return -1
+
+
+    return cnt
+
+def solution(nums: List, k: int) -> int:
+    heap = []
+    cnt = 0
+    for i in nums:
+        heapq.heappush(heap, i)
+
+    while heap[0] < k:
+
+        heapq.heappush(heap, scouvile(heapq.heappop(heap), heapq.heappop(heap)))
+        cnt += 1
+        # 시간복잡도 에러
+        # if len(heap) == 1 and heap.pop() > k:
+        #     return -1
+        if len(heap) == 1:
+            break
+
+    if heap[-1] < k:
+        return -1
+
+    return cnt
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 9, 10, 12]
+    k = 7
+    print(solution(nums, k))
