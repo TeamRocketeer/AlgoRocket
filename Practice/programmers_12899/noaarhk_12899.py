dfecc71dc77033d4c8b2f2c32f84ade53291ef03
Noaarhk <skarb8277@naver.com>
2021-05-19 17:41:56 +0900

    0519_noaarhk_revise

--- Practice/programmers_12899/noaarhk_12899.py
+++ Practice/programmers_12899/noaarhk_12899.py
@@ -1,78 +1,37 @@
-import collections
-from math import ceil
 from typing import List
 
 
-# def _put_num(nl: collections.deque, m: int):
-#     if m == 0:
-#         nl.appendleft('4')
-#     else:
-#         nl.appendleft(m)
-
-
-def _put_num(nl: List, m: int):
-    if m == 0 or m == 3:
-        nl.append('4')
+def _put_in(num_list: List, num: int):
+    if num == 3 or num == 0:
+        num_list.append(4)
     else:
-        nl.append(m)
+        num_list.append(num)
 
-# def solution(n: int ):
-#
-#    pass
-#
-def solution(n: int) -> str:
+
+def solution(n: int):
     stack = []
     answer = []
-    if n <= 3:
-        _put_num(stack, n)
-    else:  # n > 3
-        m = n
-        remainer = 0
-        while m > 3:
-            remainer = 3 if not remainer else m % 3
-            # remainer = m % 3
-            # if remainer == 0:
-            #     remainer = 3
-            _put_num(stack, remainer)
+    num = n
+    remainder = num % 3
+    _put_in(stack, remainder)
 
-            m -= remainer
-        _put_num(stack, m // 3)
+    if not remainder:
+        num -= 3
+    else:
+        num -= remainder
+
+    share = num // 3
+    while share > 3:
+        stack.append(4)
+        share -= 3
+
+    if share:
+        _put_in(stack, share)
 
     while stack:
         answer.append(str(stack.pop()))
 
-    return ''.join(answer)
-
-
-# stack = []
-# answer = []
-# while 1:
-#     _put_num(stack, remainder)
-#     share -= 3
-#     if share == 0:
-#
-# while stack:
-#     answer.append(str(stack.pop()))
-#
-#
-# # while share:
-# #     _put_num(stack, remainder)
-# #     remainder = share % 3
-# #     share //= 3
-# #
-# # _put_num(stack, remainder)
-# #
-# # while stack:
-# #     answer.append(str(stack.pop()))
-#
-# # while share:
-# #     remainder = share % 3
-# #     _put_num(num_list, remainder)
-# #     share //= 3
-# #
-# # _put_num(num_list, remainder)
-# #
-# return ''.join(answer)
+    return str(int(''.join(answer)))
 
 
 if __name__ == '__main__':
