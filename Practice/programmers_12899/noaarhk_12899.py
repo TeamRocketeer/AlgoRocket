4f6946b6f45ce94b358f0a0991a9ba7feeee93f9
Noaarhk <skarb8277@naver.com>
2021-05-20 12:02:53 +0900

    0519_noaarhk_complete

--- Practice/programmers_12899/noaarhk_12899.py
+++ Practice/programmers_12899/noaarhk_12899.py
@@ -1,39 +1,22 @@
-from typing import List
-
-
-def _put_in(num_list: List, num: int):
-    if num == 3 or num == 0:
-        num_list.append(4)
-    else:
-        num_list.append(num)
-
 
 def solution(n: int):
-    stack = []
-    answer = []
     num = n
-    remainder = num % 3
-    _put_in(stack, remainder)
-
-    if not remainder:
-        num -= 3
-    else:
-        num -= remainder
-
-    share = num // 3
-    while share > 3:
-        stack.append(4)
-        share -= 3
-
-    if share:
-        _put_in(stack, share)
-
+    stack = []
+    while num:
+        remainder = num % 3
+        if remainder == 0:
+            remainder = 4
+            num -= 1
+        stack.append(remainder)
+        num //= 3
+    answer = []
     while stack:
         answer.append(str(stack.pop()))
 
-    return str(int(''.join(answer)))
+    return ''.join(answer)
+
 
 
 if __name__ == '__main__':
-    n = 13
+    n = 6
     print(solution(n))
