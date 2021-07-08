d60c0195dacae52359c58d5488989c5b4bcd9ccf
Noaarhk <skarb8277@naver.com>
2021-07-08 11:06:04 +0900

    Complete yesterday prob

--- magic_square_noaarhk.py
+++ magic_square_noaarhk.py
@@ -78,14 +78,36 @@ class Solution:
                         answer += 1
                     y -= 1
 
-
         return answer
 
+    def solution_3(self, dir: str) -> int:
+        # visited에 점이 아닌 경로 (두 점의 조합 ) 를 추가한다.
+        visited = set()
+        x, y = 0, 0
+        for p in dir:
+            # 아래코드는 ((좌표1) -> (좌표2) ) != ((좌표2) -> (좌표1)) 로 인식하지만 문제조건상 같도록 인식해야 하기때문에
+            # 출발 -> 도착이 아닌 (작은수) -> (큰수) 로 설정한다.
+            if p == 'U' and y < 5:
+                visited.add(((x, y), (x, y + 1)))
+                y += 1
+            elif p == 'D' and y > -5:
+                visited.add(((x, y - 1), (x, y)))
+                y -= 1
+            elif p == 'L' and x > -5:
+                visited.add(((x - 1, y), (x, y)))
+                x -= 1
+            elif p == 'R' and x < 5:
+                visited.add(((x, y), (x + 1, y)))
+                x += 1
+
+        print(visited)
+        return len(visited)
 
 
 if __name__ == '__main__':
     # dir = "LULLLLLLU"
-    dir = "ULURRDLLU"
+    # dir = "ULURRDLLU"
+    dir = "UDU"
     answer = 7
     sol = Solution()
-    print(sol.solution_2(dir) == answer)
+    print(sol.solution_3(dir) == answer)
